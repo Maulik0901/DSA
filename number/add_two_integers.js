fc8bde1f47d527e72e635ac481b9319fd9c67326
Maulik0901 <maulikpatel0901@gmail.com>
2025-05-05 09:09:49 +0530

    sum of two integer

---
+++ number/add_two_integers.js
@@ -0,0 +1,32 @@
+/**
+ * Problem Statement :
+
+    Given two integers num1 and num2, return the sum of the two integers.
+
+    Example 1:
+        Input: num1 = 12, num2 = 5
+        Output: 17
+        Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+    
+    Example 2:
+        Input: num1 = -10, num2 = 4
+        Output: -6
+        Explanation: num1 + num2 = -6, so -6 is returned.
+
+    Constraints: • 
+        -100 <= num1, num2 <= 100
+ */
+
+var sum = function(num1, num2) {
+    if((num1 < -100 || num1 > 100) || (num2 < -100 || num2 > 100)) {
+        return 0 
+    }
+
+    const ans = num1 + num2;
+    return ans;
+}; 
+
+console.log(sum(-10,-20))
+console.log(sum(10,-20))
+console.log(sum(-10,20))
+console.log(sum(10,20))
